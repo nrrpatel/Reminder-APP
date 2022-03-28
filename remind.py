719cfaa80342554bb558a2b65e619df2e59bdac2
Nikunj Patel <96856377+nrrpatel@users.noreply.github.com>
2022-03-28 01:09:24 -0400

    Add files via upload
    Reminder App for a healthy lifestyle

---
+++ remind.py
@@ -0,0 +1,26 @@
+from email import message
+from operator import truediv
+from socket import timeout
+import time
+from turtle import color
+from plyer import notification
+
+if __name__ == '__main__':
+    while True:
+        notification.notify(
+            title = "Please Stand Up and Walk!!",
+            message = "To reduce the risk of a bad posture please stand up and walk around!",
+            timeout=20
+
+        )
+        time.sleep(60*30)
+
+        notification.notify(
+            title = "Please Drink Water",
+            message = "Staying hydrated is essential to a healthy lifestyle",
+            timeout=20
+
+        )
+        time.sleep(60*30)
+
+        
